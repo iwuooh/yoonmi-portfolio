77015617c389ac188831fe671710225e56cf32e6
iwuooh <dbsal06@daum.net>
2020-12-26 16:25:13 +0900

    toggle 버튼 수정, 미디어쿼리

--- js/common.js
+++ js/common.js
@@ -3,15 +3,11 @@ $(function(){
   $(".toggle_btn").click(function(){
     $("#aside").addClass('on');
     $("#aside").removeClass('off');
-    $(".close_btn").css("display","inline-block");
-    $("#main_container").css({"width":"100%"},{"left":"0%"});
     return false;
   });
   $(".close_btn").click(function(){
     $("#aside").addClass('off');
     $("#aside").removeClass('on');
-    $(".toggle_btn").css("display","inline-block");
-    // $("#main_container").css({"width":"80%"},{"left":"20%"});
     return false;
   });
 });// document ready
